ab049125c5addf6cf82d783a4deced02ed0f8f57
william <williamwambugu663@gmail.com>
2025-05-28 07:50:09 +0300

    creating tables

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,8 @@
 from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import session
+from sqlalchemy import create_engine
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -15,15 +17,46 @@ class Company(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     founding_year = Column(Integer())
-
+    freebies = relationship('Freebie', backref=backref('company', overlaps="company,freebies"))
+    devs = relationship('Dev', secondary='freebies', backref=backref('companies', overlaps="company,freebies"), overlaps="company,freebies")
     def __repr__(self):
         return f'<Company {self.name}>'
 
+    def give_freebie(self, dev, item_name, value):
+        freebie = Freebie(item_name=item_name, value=value, dev=dev, company=self)
+        return freebie
+
+    @classmethod
+    def oldest_company(cls):
+        engine = create_engine('sqlite:///freebies.db')
+        session = session(engine)
+        return session.query(cls).order_by(cls.founding_year).first()
+        
 class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
     name= Column(String())
-
+    freebies = relationship('Freebie', backref=backref('dev', overlaps="companies,devs"), overlaps="companies,devs")
     def __repr__(self):
         return f'<Dev {self.name}>'
+    
+    def received_one(self, item_name):
+        return any(freebie.item_name == item_name for freebie in self.freebies)
+    
+    def give_away(self, dev, freebie):
+        if freebie.dev == self:
+            freebie.dev = dev
+        return freebie
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+
+    def print_details(self):
+        return f'{self.dev.name} owns a {self.item_name} from {self.company.name}'
